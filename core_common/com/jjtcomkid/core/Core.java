9f1222a8f07cb3ac8a582f7f74f0a6b101473bd5
Jackson Turner <jjtcomkid@gmail.com>
2013-05-16 16:41:52 -0500

    Moved block overrides to core

---
+++ core_common/com/jjtcomkid/core/Core.java
@@ -0,0 +1,9 @@
+package com.jjtcomkid.core;
+
+import com.jjtcomkid.core.handler.LogHandler;
+
+public class Core {
+
+	public static final LogHandler logger = new LogHandler("Core");
+
+}
---
+++ core_common/com/jjtcomkid/core/handler/OverrideHandler.java
@@ -0,0 +1,37 @@
+package com.jjtcomkid.core.handler;
+
+import java.lang.reflect.Field;
+
+import net.minecraft.block.Block;
+
+import com.jjtcomkid.core.Core;
+
+public class OverrideHandler {
+	public static String getBlockName(Block block) {
+		try {
+			for (Field blockField : Block.class.getDeclaredFields()) {
+				if (blockField.getType() == Block.class && blockField.get(blockField).equals(block))
+					return blockField.getName();
+			}
+		} catch (Exception e) {
+		}
+		Core.logger.severe("Unable to retrive object name of " + block.toString());
+		return "";
+	}
+
+	public static boolean replaceBlock(Block oldBlock, Block newBlock) {
+		String oldBlockName = getBlockName(oldBlock);
+		try {
+			Field blockField = Block.class.getDeclaredField(oldBlockName);
+			blockField.setAccessible(true);
+			Field modifiers = Field.class.getDeclaredField("modifiers");
+			modifiers.setAccessible(true);
+			modifiers.setInt(blockField, blockField.getModifiers() & 0xFFFFFFEF);
+			blockField.set(Block.class, newBlock);
+			return true;
+		} catch (Exception e) {
+		}
+		Core.logger.severe("Unable to replace " + oldBlock.toString() + " with " + newBlock.toString());
+		return false;
+	}
+}
